a61f5ff7a0d4977a895af6d0b092e4f3b2f18452
ikhdark <jproonthebeat@gmail.com>
2026-01-24 23:08:50 -0600

    server

--- src/components/WhatsNew.tsx
+++ src/components/WhatsNew.tsx
@@ -1,13 +1,14 @@
 export default function WhatsNew() {
   return (
     <div className="mx-auto max-w-xl rounded-lg border bg-white p-5 text-sm shadow-sm dark:border-gray-700 dark:bg-gray-900">
-      <h2 className="mb-3 font-semibold">What’s New (Beta 1.1-1.2) </h2>
+      <h2 className="mb-3 font-semibold">What’s New (Beta 1.1-1.3) </h2>
 
       <ul className="space-y-1 text-gray-600 dark:text-gray-400">
-        <li>• Added Performance Page (MMR gap stats)</li>
-        <li>• Improved Mobile UI Experience</li>
-        <li>• Many Backend Improvements</li>
-        <li>• Added "Total Time Played vs" via Country Stats</li>
+        <li>• Added Performance Page (MMR gap stats) (1.1)</li>
+        <li>• Improved Mobile UI Experience (1.2)</li>
+        <li>• Many Backend Improvements(1.2)</li>
+        <li>• Added "Total Time Played vs" via Country Stats(1.2)</li>
+        <li>• Page loading Loading Speed Improvements (1.3)</li>
       </ul>
     </div>
   );
