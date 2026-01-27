841806e8ab1040201d8bbdb631820c12ce1e3f0e
ikhdark <jproonthebeat@gmail.com>
2026-01-27 14:23:18 -0600

    ladder

--- src/components/WhatsNew.tsx
+++ src/components/WhatsNew.tsx
@@ -1,10 +1,10 @@
 export default function WhatsNew() {
   return (
     <div className="mx-auto max-w-xl rounded-lg border bg-white p-5 text-sm shadow-sm dark:border-gray-700 dark:bg-gray-900">
-      <h2 className="mb-3 font-semibold">What’s New (Beta 1.1-1.3) </h2>
+      <h2 className="mb-3 font-semibold">What’s New (Beta 1.1-1.5) </h2>
 
       <ul className="space-y-1 text-gray-600 dark:text-gray-400">
-        <li>• Due to the new season, make sure to play ladder so your stats will show!(1.1)</li>
+        <li>• Due to the new season, make sure to play ladder so your stats will show(1.1)</li>
         <li>• Added "Performance Page" (1.1)</li>
         <li>• Improved Mobile UI Experience (1.2)</li>
         <li>• Many Backend Improvements(1.2)</li>
@@ -12,6 +12,7 @@ export default function WhatsNew() {
         <li>• Page loading Loading Speed Improvements (1.3)</li>
         <li>• Added "Time Consistency" Page (1.4)</li>
          <li>• Added Season 24 (1.5)</li>
+         <li>• Added SoS Season 24 Ladder (Global + Race) (1.5)</li>
       </ul>
     </div>
   );
